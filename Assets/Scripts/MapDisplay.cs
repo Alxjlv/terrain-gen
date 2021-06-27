a7bc11c2a89558b94d0854136f0a1c55bda8feb5
Alex Verkerk <44216323+Alxjlv@users.noreply.github.com>
2021-06-27 21:25:57 +1200

    Added basic mesh generation. Currently 2D

--- Assets/Scripts/MapDisplay.cs
+++ Assets/Scripts/MapDisplay.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 public class MapDisplay : MonoBehaviour
 {
     public Renderer textureRenderer;
+    public MeshFilter meshFilter;
+    public MeshRenderer meshRenderer;
 
     public void DrawTexture(Texture2D texture)
     {
         // Cannot use textureRenderer.material as that is only instantiated at runtime
         textureRenderer.sharedMaterial.mainTexture = texture;
         textureRenderer.transform.localScale = new Vector3(texture.width, 1, texture.height);
+    }
+
+    public void DrawMesh(MeshData meshData, Texture2D texture)
+    {
+        meshFilter.sharedMesh = meshData.CreateMesh();
+        meshRenderer.sharedMaterial.mainTexture = texture;
     }
 }
--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -7,7 +7,7 @@ public class MapGenerator : MonoBehaviour
 {
     public enum DrawMode
     {
-        NoiseMap, ColourMap
+        NoiseMap, ColourMap, Mesh
     }
 
     public DrawMode drawMode;
@@ -56,6 +56,10 @@ public class MapGenerator : MonoBehaviour
         } else if (drawMode == DrawMode.ColourMap)
         {
             display.DrawTexture(TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
+        }
+        else if (drawMode == DrawMode.Mesh)
+        {
+            display.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap), TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
         }
     }
 
